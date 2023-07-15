64ef8b4abc6b11c790c680c81ce1cab58077cb5b
MadhupriyaC <95172734+MadhupriyaC@users.noreply.github.com>
2023-07-15 17:36:33 +0530

    Init File commit

---
+++ InitialValueFile.m
@@ -0,0 +1,27 @@
+DRIVER_READINESS_HIGH = Simulink.Parameter;
+DRIVER_READINESS_HIGH.DataType = 'uint8';
+DRIVER_READINESS_HIGH.Value = 3;
+
+DRIVER_READINESS_LOW = Simulink.Parameter;
+DRIVER_READINESS_LOW.DataType = 'uint8';
+DRIVER_READINESS_LOW.Value = 1;
+
+DRIVER_READINESS_MEDIUM = Simulink.Parameter;
+DRIVER_READINESS_MEDIUM.DataType = 'uint8';
+DRIVER_READINESS_MEDIUM.Value = 2;
+
+DRIVER_SEATED_ON = Simulink.Parameter;
+DRIVER_SEATED_ON.DataType = 'boolean';
+DRIVER_SEATED_ON.Value = 1;
+
+DRIVER_SEATED_OFF = Simulink.Parameter;
+DRIVER_SEATED_OFF.DataType = 'boolean';
+DRIVER_SEATED_OFF.Value = 0;
+
+DRIVER_EYES_ON = Simulink.Parameter;
+DRIVER_EYES_ON.DataType = 'boolean';
+DRIVER_EYES_ON.Value = 1;
+
+DRIVER_EYES_OFF = Simulink.Parameter;
+DRIVER_EYES_OFF.DataType = 'boolean';
+DRIVER_EYES_OFF.Value = 0;
